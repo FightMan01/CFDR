3aed8875a981c57da89438c69055148fd2aeae65
FightMan01 <04aron@gmail.com>
2026-03-19 19:28:11 +0100

    fix(cfdr): remove unused font_size variable from CFDR_UI_State and add device pixel ratio function to alice_canvas.js

--- src/cfdr_sze_portal/alice_canvas.js
+++ src/cfdr_sze_portal/alice_canvas.js
@@ -273,6 +273,10 @@ function js_web_current_url(url_cap, url_ptr) {
   }
 }
 
+function js_web_device_pixel_ratio() {
+  return window.devicePixelRatio || 1;
+}
+
 // ------------------------------------------------------------
 // #-- NOTE(cmat): JS - WASM platform API.
 
@@ -889,6 +893,7 @@ function wasm_module_load(wasm_bytecode) {
 
       // NOTE(cmat): Web-specific API
       js_web_current_url:             js_web_current_url,
+      js_web_device_pixel_ratio:      js_web_device_pixel_ratio,
 
       // NOTE(cmat): Platform API.
       js_pl_set_shared_memory:  js_pl_set_shared_memory,
@@ -1013,4 +1018,3 @@ function wasm_module_load(wasm_bytecode) {
 fetch("alice_canvas.wasm")
   .then(response => response.arrayBuffer())
   .then(bytes    => wasm_module_load(bytes));
-
