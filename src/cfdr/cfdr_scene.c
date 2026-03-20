83d06a615fd53ecdf5d4dfea08f2a7b7a1d7b125
Matyas Constans <mconstanshu@gmail.com>
2026-03-20 20:13:05 +0800

    prototype

--- src/cfdr/cfdr_scene.c
+++ src/cfdr/cfdr_scene.c
@@ -2,6 +2,7 @@ fn_internal void cfdr_scene_init(CFDR_Scene *object) {
 }
 
 fn_internal CFDR_Object_Node *cfdr_scene_push(CFDR_Scene *object) {
+  return 0;
 }
 
 fn_internal void cfdr_scene_draw(CFDR_Scene *object, R2F draw_region) {
